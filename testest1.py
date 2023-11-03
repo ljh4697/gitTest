5b26c2001167eebab0b8bc75ba99ce10fee113f9
이준혁(Lee Joonhyeok)(ljhljh4697) <ljhljh4697@ncsoft.com>
2023-11-03 11:29:41 +0900

    push test

--- testest1.py
+++ testest1.py
@@ -1,15 +1,44 @@
-b = " o t her "
-k="ee"
-c = "bbbbb"
-d = 'xxxxx'
+
+tagmer = {
+    "neutral": '평범한 목소리',  # Neutral (무감정)
+    "happines": '기쁜 목소리',  # Happiness (행복)
+    "fear": '겁 먹은 목소리',  # Fear (공포)
+    "dislike": '싫은 목소리',  # Disgust (역겨움)
+    "anger": '화난 목소리',   # Anger (화남)
+    "sadness": '슬픈 목소리',  # Sadness (슬픔)
+    "surprise": '놀란 목소리'  # Surprise (놀람)
+}
 
 
-f = 'ggggg'
+merInput={
+    'elapsed_time': '0.29805469512939453',
+    'text': '오늘 날씨 어때?',
+    'output': [{'neutral': 0.8293156027793884}, 
+        {'happines': 0.19121423363685608},
+        {'fear': 0.15142793953418732},
+        {'dislike': 0.14946505427360535},
+        {'anger': 1.14812247455120087},
+        {'sadness': 0.1437532901763916},
+        {'surprise': 0.14216795563697815}]
+    }    
 
-print(b.strip()[0])
-print(b.strip()[-1])
-print(b.strip())
 
+
+emotion_voice = 'neutral'
+mer_activation = 0
+
+
+for i, emotion in enumerate(merInput['output']):
+    for k in emotion.keys():
+        if emotion[k]>mer_activation:
+            mer_activation = emotion[k]
+            emotion_voice = tagmer[k]
+            
+            
+print(emotion_voice)
+print(mer_activation)
+        
+        
 
 # eeee11111
 ## rr
